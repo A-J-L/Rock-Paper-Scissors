38652d8c0f1a9838088081df31eb8bff099a8273
A-J-L <72743590+A-J-L@users.noreply.github.com>
2025-07-24 20:35:17 +0100

    Completed step 5 and started step 6 - moved playRound function and scorer variables so that they're inside the new playGame function

--- javascript.js
+++ javascript.js
@@ -13,17 +13,16 @@ for (let i = 1; i <= 3; i++) {
   console.log(randomNum);
   }
 }
-
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 
 //Logic to get Human Choice
 function getHumanChoice() {
   let userChoice = prompt("Enter either; rock, paper, or scissors");
-  return userChoice
+  return userChoice;
 }
 
- console.log(getHumanChoice());
+ //console.log(getHumanChoice());
 
  //Step 4: Tracking players score
  let humanScore = 0;
@@ -31,14 +30,51 @@ function getHumanChoice() {
 
 
  //Step 5: Logic for playing a single round
- function playRound(humanChoice, computerChoice) {
-    //humanChoice.toLowerCase();
-    console.log("You lose! Paper beats Rock");
+ 
+
+
+
+ 
+ //Step 6: Logic to play the entire game
+function playGame() {
+  function playRound(humanChoice, computerChoice) {
+    //console.log(humanChoice = humanChoice.toLowerCase());
+    if (humanChoice === "rock" && computerChoice ==="scissors"
+    ) {
+      console.log("You win! Rock beats scissors");
+      humanScore += 1;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("You lose! Paper beats rock");
+      computerScore += 1;
+    } else if (humanChoice === "rock" && computerChoice === "rock") {
+      console.log("Draw!");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You win! Scissors beats paper");
+      humanScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("You lose! Rock beats scissors");
+      computerScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+      console.log("Draw!");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("You lose! Scissors beats paper");
+      computerScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats rock");
+      humanScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+      console.log("Draw!")
+    } else {
+      console.log("Try again");
+    }
  }
+}
 
  const humanSelection = getHumanChoice();
  const computerSelection = getComputerChoice();
 
- console.log(playRound(humanSelection, computerSelection));
 
- //Step 6: Logic to play the entire game
+//Testing for step 5
+playRound(humanSelection, computerSelection);
+console.log(humanScore);
+console.log(computerScore);
